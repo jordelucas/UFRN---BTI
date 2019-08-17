b66ad38cb519191fcaa3e7da7b10ab920735adbe
jordelucas <jordevalucas@hotmail.com>
2019-08-17 18:07:40 -0300

    finalizaçao do jantar dos filosofos

--- aula09/filosofos.py
+++ aula09/filosofos.py
@@ -2,6 +2,11 @@ from threading import Thread
 from random import randint
 import time
 
+garfos = [0, 0, 0, 0, 0]
+
+def pensar(id):
+	print("Filósofo ", id+1, ": pensando")
+	time.sleep(randint(1,10))
 
 def comer(id):
 	esquerda = 0
@@ -11,34 +16,37 @@ def comer(id):
 	else:
 		esquerda = id - 1
 
-	print("filosofo ", id+1, "|direita: ", direita, "|esquerda: ", esquerda)
+	while True:
+		if (garfos[esquerda] == 0) and (garfos[direita] == 0):
+			garfos[direita] = 1
+			garfos[esquerda] = 1
+			print("Filosofo ", id+1, ": comendo")
+			time.sleep(randint(1,10))
+			garfos[esquerda] = 0
+			garfos[direita] = 0
+			pensar(id)
+			break
+		else:
+			print("filosofo ", id+1, ": aguardando")
+			time.sleep(randint(1,10))
 
-	
+def inicioJantar(id):
+	while True:
+		escolha = randint(0,1)
 
-	if (garfos[esquerda] == 0) and (garfos[direita] == 0):
-		print("filosofo ", id+1, " comendo")
-		garfos[esquerda] = 1
-		garfos[direita] = 1
-		time.sleep(randint(0,9))
-		print("filosofo ", id+1, " pensando")
-		garfos[esquerda] = 0
-		garfos[direita] = 0
-	else:
-		print("filosofo ", id+1, " aguardando")
-		time.sleep(randint(0,9))
-	
+		if escolha == 0:
+			pensar(id)
+		else:
+			comer(id)
 
-garfos = [0, 0, 0, 0, 0]
+filosofo1 = Thread(target=inicioJantar,args=[0])
+filosofo2 = Thread(target=inicioJantar,args=[1])
+filosofo3 = Thread(target=inicioJantar,args=[2])
+filosofo4 = Thread(target=inicioJantar,args=[3])
+filosofo5 = Thread(target=inicioJantar,args=[4])
 
-filosofo1 = Thread(target=comer,args=[0])
-filosofo2 = Thread(target=comer,args=[1])
-filosofo3 = Thread(target=comer,args=[2])
-filosofo4 = Thread(target=comer,args=[3])
-filosofo5 = Thread(target=comer,args=[4])
-
-while True:
-	filosofo1.start()
-	filosofo2.start()
-	filosofo3.start()
-	filosofo4.start()
-	filosofo5.start()
+filosofo1.start()
+filosofo2.start()
+filosofo3.start()
+filosofo4.start()
+filosofo5.start()
