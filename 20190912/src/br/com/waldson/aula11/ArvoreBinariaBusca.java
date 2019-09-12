b97a938e4f1b4660c7ea43f3f3ad78e9975d5f07
jordelucas <jordevalucas@hotmail.com>
2019-09-12 20:38:00 -0300

    add interface grafica arvore

---
+++ 20190912/src/br/com/waldson/aula11/ArvoreBinariaBusca.java
@@ -0,0 +1,37 @@
+package br.com.waldson.aula11;
+
+public class ArvoreBinariaBusca {
+    private Node raiz = null;
+    private Node left = null;
+    private Node right = null;
+
+
+    public boolean isEmpty() {
+        return raiz == null;
+    }
+
+    public Node getRoot() {
+        return raiz;
+    }
+
+    public void insert(Node node) {
+        if (raiz == null) {
+            raiz = node;
+            return;
+        }
+        raiz.insert(node);
+    }
+
+    public void insert(int value) {
+        Node n = new Node(value);
+        insert(n);
+    }
+
+    public Node search(int key) {
+        if (raiz == null) {
+            return null;
+        }
+        return raiz.search(key);
+    }
+
+}
