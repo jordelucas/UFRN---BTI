d1c65fe817f9758bf763124be4223ed9facd77a5
Jordevá Lucas <jordeva.silva@imd.ufrn.br>
2019-09-19 12:31:26 -0300

    ajustes abb - exclusao

--- 20190917atv/src/br/com/waldson/aula11/Main.java
+++ 20190917atv/src/br/com/waldson/aula11/Main.java
@@ -7,16 +7,18 @@ public class Main {
 
     public static void main(String[] args) {
         ArvoreBinariaBusca arvore = new ArvoreBinariaBusca();
-        arvore.insert(6);
+        arvore.insert(9);
         arvore.insert(1);
-        arvore.insert(7);
+        arvore.insert(10);
+        arvore.insert(6);
         arvore.insert(2);
         arvore.insert(5);
+        arvore.insert(3);
+        arvore.insert(4);
+        arvore.insert(7);
         arvore.insert(0);
 
-        arvore.remove(2);
-        arvore.remove(0);
-        arvore.remove(5);
+        arvore.remove(6);
 
         JFrame frame = new JFrame("Visualizador de ABB");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
--- 20190917atv/src/br/com/waldson/aula11/Node.java
+++ 20190917atv/src/br/com/waldson/aula11/Node.java
@@ -66,16 +66,33 @@ public class Node {
         if (key < this.value) {
             if (this.left != null) {
                 if (this.left.value == key) {
+                    //folha
                     if(leaf(this.left)) {
                         this.left = null;
                         return this;
                     }
 
+                    //um filho
                     Node temp = one(this.left);
                     if (temp != null) {
                         this.left = temp;
                         return this;
                     }
+
+                    //dois filhos
+                    temp = this.left;
+                    if(temp.left.right == null) {
+                        temp.left.insert(temp.right);
+                        this.left = temp.left;
+                        return this;
+                    }
+
+                    temp = this.left.left;
+                    Node maiorValor = maiorValor(this.left.left);
+                    maiorValor.insert(this.left.right);
+                    maiorValor.insert(this.left.left);
+                    this.left = maiorValor;
+                    return this;
                 } else {
                     return this.left.remove(key);
                 }
@@ -85,16 +102,33 @@ public class Node {
         if (key > this.value) {
             if (this.right != null) {
                 if (this.right.value == key) {
+                    //folha
                     if(leaf(this.right)) {
                         this.right = null;
                         return this;
                     }
 
+                    //um filho
                     Node temp = one(this.right);
                     if (temp != null) {
                         this.right = temp;
                         return this;
                     }
+
+                    //dois filhos
+                    temp = this.right;
+                    if(temp.left.right == null) {
+                        temp.left.insert(temp.right);
+                        this.right = temp.left;
+                        return this;
+                    }
+
+                    temp = this.right.left;
+                    Node maiorValor = maiorValor(this.right.left);
+                    maiorValor.insert(this.right.right);
+                    maiorValor.insert(this.right.left);
+                    this.right = maiorValor;
+                    return this;
                 } else {
                     return this.right.remove(key);
                 }
@@ -119,4 +153,15 @@ public class Node {
     public boolean leaf (Node node) {
         return node.left == null && node.right == null;
     }
+
+    public Node maiorValor (Node node) {
+        if (node.right.right == null) {
+            Node temp = node.right;
+            node.right = null;
+            return temp;
+        }
+
+        return maiorValor(node.right);
+    }
+
 }
